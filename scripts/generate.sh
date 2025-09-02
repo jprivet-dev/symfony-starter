5cf79936cae762ca93742f3c8b9abe60ac3ce3eb
jprivet-dev <jprivet.dev@gmail.com>
2025-09-02 08:58:55 +0200

    feat(generate.sh): add docker clear cache & remove push force

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -17,27 +17,24 @@ function generate_version() {
     local command="${2}"
     local branch="${3}"
 
-    printf "\n${Y}Generate new version${S}"
-    printf "\n${Y}--------------------${S}\n\n"
+    printf "\n"
+    printf " ${Y}>${S}\n"
+    printf " ${Y}>${S} Generate ${command} version in ${branch} branch\n"
+    printf " ${Y}>${S}\n"
+    printf "\n"
 
-    printf " ${Y}>${S}\n"
-    printf " ${Y}>${S} ${command} version\n"
-    printf " ${Y}>${S}\n"
+    docker compose down --volumes --rmi all
 
     if git rev-parse --verify --quiet "${branch}" >/dev/null; then
         printf " ${R}⨯${S} ${branch} branch already exists. Remove it.\n"
         git branch -D "${branch}"
     fi
 
-    git new "${branch}"
-    git checkout "${branch}"
-
+    git checkout -b "${branch}"
     make "${command}"
 
     git add .
     git commit -m "Save ${command} version"
-    git push origin "${branch}" --force-with-lease
-
     printf " ${G}✔${S} ${command} version saved & pushed\n"
 
     git checkout "${current}"
