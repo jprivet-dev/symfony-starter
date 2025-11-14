ec23a92865940c23573cdf457cb9200a29490484
jprivet-dev <jprivet.dev@gmail.com>
2025-11-14 08:51:25 +0100

    feat(local.mk): update examples & remove unnecessary docker-stop-all.sh

--- scripts/docker-stop-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Stop all running Docker containers at once (use with caution!).
-#
-# Usage:
-#   . script/docker-stop-all.sh
-# or
-#   source script/docker-stop-all.sh
-
-docker stop $(docker ps -a -q)
