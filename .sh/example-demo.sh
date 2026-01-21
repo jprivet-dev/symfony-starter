b0ec879fb678ae5443dbfc5b7071a6b1eb362393
jprivet-dev <jprivet.dev@gmail.com>
2026-01-21 12:03:34 +0100

    feat(example-demo.sh): create base of file

---
+++ .sh/example-demo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script allows you to test and generate a sample application, with one commit per step.
+#
+# Usage:
+#   . .sh/example-demo.sh
+# or
+#   source .sh/example-demo.sh
+
+make clone_symfony_demo
+git add . && git commit -m "make clone_symfony_demo"
+
+make clone_symfony_docker
+git add . && git commit -m "make clone_symfony_docker"
+
+make _patch_var_log_mapping
+git add . && git commit -m "make _patch_var_log_mapping"
+
+make _patch_sqlite_base
+git add . && git commit -m "make _patch_sqlite_base"
+
+make build
+
+make up_detached
+git add . && git commit -m "make up_detached"
+
+make runtime
+make permissions
+make images
+make info
